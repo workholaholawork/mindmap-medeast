4362c494eac3c0fceeadf23d890aa963629cf9e1
workholaholawork <work.hola.hola.work@gmail.com>
2026-04-01 23:03:53 +0300

    Add files via upload

---
+++ statsionar/statsionar.mm
@@ -0,0 +1,3 @@
+<map version="1.0.1">
+<node TEXT="6. Стационар" BACKGROUND_COLOR="#e8f0fe" COLOR="#1f2937" FOLDED="false"><node TEXT="Дневной стационар" FOLDED="false"></node><node TEXT="Круглосуточный стационар" FOLDED="false"></node></node>
+</map>
